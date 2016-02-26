78a69ae52de8ed8b43149fa48f277b9ef75c50b6
hankgielarowski <w_gielarowski@yahoo.com>
2016-02-26 11:01:23 -0500

    removed doubleation

--- chatroom/templates.js
+++
@@ -1,9 +0,0 @@
-var templates = {
-  chats: [
-  '<div data-idx="<%= _id %>">',
-  '<h5 class="usersName" ><%= username %></h5>',
-  '<p><%= post %></p>',
-  '<button type="button" name="Delete" class="delete-post">x</button>',
-  '</div>'
-].join("")
-};
